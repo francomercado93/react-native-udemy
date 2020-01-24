55571e89e359181085a1eceb6e6ab9b743631afc
francomercado93 <mercadofranco.a@gmail.com>
2020-01-24 15:10:55 -0300

    Refactor de CounterScreen para usar un Reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,26 +1,66 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import React, { useReducer } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
+const COUNTER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+
+    // state === { counter: 0 }
+    // action === { type: 'change_counter', payload: 1 }
+    switch (action.type) {
+        case 'increment':
+            return { ...state, counter: state.counter + action.payload }
+        case 'decrement':
+            return { ...state, counter: state.counter - action.payload }
+        default:
+            return state;
+    }
+    return
+}
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+
     return (
         <View>
             <Button
                 title="Sumar"
                 onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({ type: 'increment', payload: COUNTER_INCREMENT });
                 }}
             />
             <Button
                 title="Restar"
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({ type: 'decrement', payload: COUNTER_INCREMENT });
                 }}
             />
-            <Text>Contador: {counter}</Text>
+            <Text>Contador: {state.counter}</Text>
         </View>
     )
 }
+
+// const CounterScreen = () => {
+//     const [counter, setCounter] = useState(0);
+
+//     return (
+//         <View>
+//             <Button
+//                 title="Sumar"
+//                 onPress={() => {
+//                     setCounter(counter + 1);
+//                 }}
+//             />
+//             <Button
+//                 title="Restar"
+//                 onPress={() => {
+//                     setCounter(counter - 1);
+//                 }}
+//             />
+//             <Text>Contador: {counter}</Text>
+//         </View>
+//     )
+// }
 
 const styles = StyleSheet.create({})
 
